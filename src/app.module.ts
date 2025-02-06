cc529b128927c937340ae4250952e9a9a92adef7
alaeddinerami <alaeddinerami7@gmail.com>
2025-02-06 15:25:25 +0100

    feat: integrate MongoDB connection with ConfigModule
    Added Mongoose integration with async configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'), 
+      }),
+      inject: [ConfigService],
+    }),
+
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
