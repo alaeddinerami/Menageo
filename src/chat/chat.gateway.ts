c29f4a294d843380122ff5858b49ff632a5ebf37
alaeddinerami <alaeddinerami7@gmail.com>
2025-02-28 15:14:55 +0000

    MEN-7 commit meddelware

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -13,24 +13,24 @@ export class ChatGateway {
 
   constructor(
     private readonly jwtService: JwtService,
-    private readonly chatService: ChatService,
+    private readonly chatService: ChatService,  
   ) {}
 
-  afterInit() {
-    this.server.use((socket, next) => {
-      const token = socket.handshake.auth.token || socket.handshake.headers.authorization?.split(' ')[1];
-      if (!token) {
-        return next(new WsException('No token provided'));
-      }
-      try {
-        const payload = this.jwtService.verify(token);
-        socket.data.userId = payload.sub;
-        next();
-      } catch (error) {
-        next(new WsException('Invalid token'));
-      }
-    });
-  }
+  // afterInit() {
+  //   this.server.use((socket, next) => {
+  //     const token = socket.handshake.auth.token || socket.handshake.headers.authorization?.split(' ')[1];
+  //     if (!token) {
+  //       return next(new WsException('No token provided'));
+  //     }
+  //     try {
+  //       const payload = this.jwtService.verify(token);
+  //       socket.data.userId = payload.sub;
+  //       next();
+  //     } catch (error) {
+  //       next(new WsException('Invalid token'));
+  //     }
+  //   });
+  // }
 
   handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}, User: ${client.data.userId}`);
@@ -42,9 +42,9 @@ export class ChatGateway {
 
   @SubscribeMessage('joinChat')
   async handleJoinChat(client: Socket, receiverId: string) {
-    const room = this.getDiscationName(client.data.userId, receiverId);
-    client.join(room);
-    console.log(`${client.data.userId} joined room ${room}`);
+    const disc = this.getDiscutionName(client.data.userId, receiverId);
+    client.join(disc);
+    console.log(`${client.data.userId} joined discution ${disc}`);
 
     const messages = await this.chatService.getMessages(client.data.userId, receiverId);
     client.emit('chatHistory', messages);
@@ -52,8 +52,8 @@ export class ChatGateway {
 
   @SubscribeMessage('message')
   async handleMessage(client: Socket, payload: { receiverId: string; content: string }) {
-    const senderId = client.data.userId;
-    const room = this.getDiscationName(senderId, payload.receiverId);
+    const senderId = 'kqgv55Vo2mRfJiVNAAAB';
+    const disc = this.getDiscutionName(senderId, payload.receiverId);
 
     const createChatDto: CreateChatDto = {
       senderId,
@@ -63,7 +63,7 @@ export class ChatGateway {
 
     const savedMessage = await this.chatService.saveMessage(createChatDto);
 
-    this.server.to(room).emit('message', {
+    this.server.to(disc).emit('message', {
       id: savedMessage._id,
       senderId,
       receiverId: payload.receiverId,
@@ -72,7 +72,7 @@ export class ChatGateway {
     });
   }
 
-  private getDiscationName(userId1: string, userId2: string): string {
+  private getDiscutionName(userId1: string, userId2: string): string {
     return [userId1, userId2].sort().join('_'); 
   }
 }
