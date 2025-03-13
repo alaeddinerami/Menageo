678ab87bef99c32f2296ad54fd85a79a8acf4e8d
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-13 15:25:19 +0000

    Refactor Chat and Message entities for better data handling

--- src/chat/entities/chat.entity.ts
+++ src/chat/entities/chat.entity.ts
@@ -1,21 +1,35 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, HydratedDocument } from 'mongoose';
+import { Document } from 'mongoose';
 
-export type MessageDocument = HydratedDocument<Message>;
-
-@Schema({ timestamps: true })
+@Schema()
 export class Message extends Document {
   @Prop({ required: true })
-  senderId: string; 
-
-  @Prop({ required: true })
-  receiverId: string; 
+  senderId: string;
 
   @Prop({ required: true })
   content: string;
 
   @Prop({ default: false })
   isRead: boolean;
+
+  @Prop({ default: Date.now })
+  timestamp: Date;
+
 }
 
 export const MessageSchema = SchemaFactory.createForClass(Message);
+
+@Schema({ timestamps: true })
+export class Chat extends Document {
+  @Prop({ required: true })
+  user1: string;
+
+  @Prop({ required: true })
+  user2: string;
+
+  @Prop({ type: [MessageSchema], default: [] })
+  messages: Message[];
+}
+
+export type ChatDocument = Chat;
+export const ChatSchema = SchemaFactory.createForClass(Chat);
