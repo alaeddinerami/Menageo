788940926ded28f5528bbaee2a910daf97d0ee11
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-13 15:25:10 +0000

    Update CreateChatDto for chat creation request structure

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,15 +1,15 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateChatDto {
-    @IsString()
     @IsNotEmpty()
+    @IsString()
     senderId: string;
-
-    @IsString()
+  
     @IsNotEmpty()
+    @IsString()
     receiverId: string;
-
-    @IsString()
+  
     @IsNotEmpty()
+    @IsString()
     content: string;
 }
