e189949a855438ae6c5b0583a020aaf026ff97b5
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 23:23:26 +0000

    Mise à jour du fichier principal

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,17 @@ import * as dotenv from 'dotenv';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-dotenv.config(); 
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule); // Type the app as NestExpressApplication
-
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Failed to start application:', err);
+  process.exit(1);
+});
