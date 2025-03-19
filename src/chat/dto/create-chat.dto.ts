02dd476296fd818a4a557a8a092bd48cbe52245f
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 23:23:19 +0000

    Mise à jour des DTOs de chat

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,15 +1,15 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateChatDto {
-    @IsNotEmpty()
-    @IsString()
-    senderId: string;
-  
-    @IsNotEmpty()
-    @IsString()
-    receiverId: string;
-  
-    @IsNotEmpty()
-    @IsString()
-    content: string;
+  @IsNotEmpty()
+  @IsString()
+  senderId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  receiverId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
 }
