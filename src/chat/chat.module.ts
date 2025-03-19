f9b8561d9ac5662679781dce639ea276bb9833bf
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 23:23:17 +0000

    Mise à jour du module de chat

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from 'src/auth/auth.module'; 
+import { AuthModule } from 'src/auth/auth.module';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
-import { ChatSchema, Chat } from './entities/chat.entity'; 
-import { MessageSchema, Message } from './entities/chat.entity'; 
+import { ChatSchema, Chat } from './entities/chat.entity';
+import { MessageSchema, Message } from './entities/chat.entity';
 import { UserSchema } from 'src/user/entities/user.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'Chat', schema: ChatSchema }, 
-      { name: 'Message', schema: MessageSchema }, 
-      { name: 'User', schema: UserSchema }, 
+      { name: 'Chat', schema: ChatSchema },
+      { name: 'Message', schema: MessageSchema },
+      { name: 'User', schema: UserSchema },
     ]),
-    AuthModule, 
+    AuthModule,
   ],
   controllers: [ChatController],
-  providers: [ChatService, ChatGateway], 
+  providers: [ChatService, ChatGateway],
 })
 export class ChatModule {}
