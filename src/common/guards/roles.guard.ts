cf2e3c0ff8d21d4259ab23b57f085b18aa11f99f
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 23:23:23 +0000

    Mise à jour du guard des rôles

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,4 +1,9 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '../enums/roles.enum';
 
@@ -7,11 +12,14 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [context.getHandler(),context.getClass()]);
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     if (!requiredRoles) {
       return true;
     }
-    
+
     const request = context.switchToHttp().getRequest();
     const user = request.user;
     // console.log(user)
