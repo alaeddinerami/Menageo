9224db769674cf8f7f7d50e45552037b90b87ee8
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 23:23:30 +0000

    Mise à jour du service de réservation

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -1,9 +1,16 @@
-import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
-import { Reservation, ReservationDocument } from './entities/reservation.entity';
+import {
+  Reservation,
+  ReservationDocument,
+} from './entities/reservation.entity';
 import { User } from 'src/user/entities/user.entity';
 import { Role } from 'src/common/enums/roles.enum';
 import { log } from 'console';
@@ -11,14 +18,18 @@ import { log } from 'console';
 @Injectable()
 export class ReservationService {
   constructor(
-    @InjectModel(Reservation.name) private reservationModel: Model<ReservationDocument>,
+    @InjectModel(Reservation.name)
+    private reservationModel: Model<ReservationDocument>,
     @InjectModel(User.name) private userModel: Model<User>,
   ) {}
 
-  async create(createReservationDto: CreateReservationDto, clientId: string): Promise<Reservation> {
+  async create(
+    createReservationDto: CreateReservationDto,
+    clientId: string,
+  ): Promise<Reservation> {
     const { cleanerId, date, duration, notes } = createReservationDto;
     // console.log('Input:', { cleanerId, date, duration, notes });
-  
+
     const requestedStart = new Date(date);
     if (isNaN(requestedStart.getTime())) {
       throw new BadRequestException('Invalid date format');
@@ -26,8 +37,10 @@ export class ReservationService {
     if (duration <= 0 || !Number.isInteger(duration)) {
       throw new BadRequestException('Duration must be a positive integer');
     }
-    const requestedEnd = new Date(requestedStart.getTime() + duration * 60 * 1000);
-  
+    const requestedEnd = new Date(
+      requestedStart.getTime() + duration * 60 * 1000,
+    );
+
     const cleaner = await this.userModel.findById(cleanerId).exec();
     if (!cleaner || !cleaner.roles.includes(Role.cleaner)) {
       throw new BadRequestException('Invalid cleaner ID');
@@ -36,25 +49,25 @@ export class ReservationService {
     if (!client) {
       throw new BadRequestException('Invalid client ID');
     }
-  
-    
-      const cleanerOverlaps = await this.reservationModel.find({
-      cleaner: cleaner._id,
-      status: { $in: ['pending', 'accepted'] },
-      $and: [
-        { date: { $lt: requestedEnd } },
-        {
-          $expr: {
-            $gt: [
-              { $add: ['$date', { $multiply: ['$Duration', 60 * 1000] }] },
-              requestedStart,
-            ],
+
+    const cleanerOverlaps = await this.reservationModel
+      .find({
+        cleaner: cleaner._id,
+        status: { $in: ['pending', 'accepted'] },
+        $and: [
+          { date: { $lt: requestedEnd } },
+          {
+            $expr: {
+              $gt: [
+                { $add: ['$date', { $multiply: ['$Duration', 60 * 1000] }] },
+                requestedStart,
+              ],
+            },
           },
-        },
-      ],
-    }).exec();
-  
-  
+        ],
+      })
+      .exec();
+
     if (cleanerOverlaps.length > 0) {
       throw new BadRequestException('Cleaner is already booked at this time');
     }
@@ -66,37 +79,46 @@ export class ReservationService {
       status: 'pending',
       Note: notes,
     });
- return await reservation.save();
+    return await reservation.save();
   }
 
   async findAll(userId: string, userRoles: Role[]): Promise<Reservation[]> {
     if (userRoles.includes(Role.Admin)) {
       return this.reservationModel.find().populate('cleaner client').exec();
     }
-    return this.reservationModel.find({ client: userId }).populate('cleaner').exec();
+    return this.reservationModel
+      .find({ client: userId })
+      .populate('cleaner')
+      .exec();
   }
 
   async findOne(id: string): Promise<Reservation[]> {
     try {
-      const reservations = await this.reservationModel.find({ cleaner: new this.reservationModel.base.Types.ObjectId(id) }).select('date').exec();
+      const reservations = await this.reservationModel
+        .find({ cleaner: new this.reservationModel.base.Types.ObjectId(id) })
+        .select('date')
+        .exec();
       // console.log('Reservations:', reservations);
-     
+
       return reservations;
     } catch (error) {
       throw new NotFoundException(error.message);
     }
   }
 
-
-  
   async findAllReservationsCleaner(userId: string): Promise<Reservation[]> {
-
-
-    return this.reservationModel.find({ cleaner: userId }).populate('client').exec();
+    return this.reservationModel
+      .find({ cleaner: userId })
+      .populate('client')
+      .exec();
   }
 
-
-  async update(id: string, updateReservationDto: UpdateReservationDto, userId: string, userRoles: Role[]): Promise<Reservation> {
+  async update(
+    id: string,
+    updateReservationDto: UpdateReservationDto,
+    userId: string,
+    userRoles: Role[],
+  ): Promise<Reservation> {
     const reservation = await this.reservationModel.findById(id).exec();
     if (!reservation) {
       throw new NotFoundException(`Reservation with ID ${id} not found`);
@@ -110,7 +132,7 @@ export class ReservationService {
     if (date || duration) {
       const requestedStart = date ? new Date(date) : reservation.date;
       const newDuration = duration ?? reservation.Duration;
-      
+
       if (isNaN(requestedStart.getTime())) {
         throw new BadRequestException('Invalid date format');
       }
@@ -118,24 +140,28 @@ export class ReservationService {
         throw new BadRequestException('Duration must be a positive integer');
       }
 
-      const requestedEnd = new Date(requestedStart.getTime() + newDuration * 60 * 1000);
+      const requestedEnd = new Date(
+        requestedStart.getTime() + newDuration * 60 * 1000,
+      );
 
-      const cleanerOverlaps = await this.reservationModel.find({
-        cleaner: reservation.cleaner,
-        _id: { $ne: reservation._id }, 
-        status: { $in: ['pending', 'accepted'] },
-        $and: [
-          { date: { $lt: requestedEnd } },//date is less than requested end
-          {
-            $expr: {
-              $gt: [
-                { $add: ['$date', { $multiply: ['$Duration', 60 * 1000] }] },
-                requestedStart,
-              ],
+      const cleanerOverlaps = await this.reservationModel
+        .find({
+          cleaner: reservation.cleaner,
+          _id: { $ne: reservation._id },
+          status: { $in: ['pending', 'accepted'] },
+          $and: [
+            { date: { $lt: requestedEnd } }, //date is less than requested end
+            {
+              $expr: {
+                $gt: [
+                  { $add: ['$date', { $multiply: ['$Duration', 60 * 1000] }] },
+                  requestedStart,
+                ],
+              },
             },
-          },
-        ],
-      }).exec();
+          ],
+        })
+        .exec();
 
       if (cleanerOverlaps.length > 0) {
         throw new BadRequestException('Cleaner is already booked at this time');
@@ -158,15 +184,22 @@ export class ReservationService {
     return updatedReservation;
   }
 
-  async remove(id: string, userId: string, userRoles: Role[]): Promise<Reservation> {
+  async remove(
+    id: string,
+    userId: string,
+    userRoles: Role[],
+  ): Promise<Reservation> {
     const reservation = await this.reservationModel.findById(id).exec();
     if (!reservation) {
       throw new NotFoundException(`Reservation with ID ${id} not found`);
     }
 
-    if (!userRoles.includes(Role.Admin) && reservation.client.toString() !== userId) {
-      throw new BadRequestException('Unauthorized to cancel this reservation');
-    }
+    // if (
+    //   !userRoles.includes(Role.Admin) &&
+    //   reservation.client.toString() !== userId
+    // ) {
+    //   throw new BadRequestException('Unauthorized to cancel this reservation');
+    // }
 
     reservation.status = 'cancelled';
     return reservation.save();
