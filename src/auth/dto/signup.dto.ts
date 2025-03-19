c44c9442c4a6429640ffb60f3f3e9ba597d658dc
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 21:43:23 +0000

    Modification du DTO pour l'inscription

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,13 +1,19 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
-export class SignupDto{
-    @IsString()
-    name: string;
+export class SignupDto {
+  @IsString()
+  name: string;
 
-    @IsEmail()
-    email:string;
+  @IsEmail()
+  email: string;
 
-    @IsString()
-    @MinLength(6,{message: "password must be 6 character"})
-    password: string;
+  @IsString()
+  @MinLength(6, { message: 'password must be 6 character' })
+  password: string;
+
+  @IsString()
+  location: string;
+  @IsString()
+  @MinLength(10, { message: 'phone must be 10 character' })
+  phone: string;
 }
