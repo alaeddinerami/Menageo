5ee64da106e83ceec8ff82cb9f69667d459c79ba
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 23:23:16 +0000

    Mise à jour du contrôleur de chat

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Post, Body, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { Chat, Message } from './entities/chat.entity'; // Message Entity
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('chat')
-@UseGuards(AuthGuard('jwt')) 
+@UseGuards(AuthGuard('jwt'))
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post('message')
-  async sendMessage(
-    @Body() createChatDto: CreateChatDto, 
-  ): Promise<Message> {
+  async sendMessage(@Body() createChatDto: CreateChatDto): Promise<Message> {
     const { senderId, receiverId, content } = createChatDto;
     return this.chatService.sendMessage(senderId, receiverId, content);
   }
@@ -29,7 +35,9 @@ export class ChatController {
     return this.chatService.getUserChats(userId);
   }
   @Patch('message/:messageId/read')
-  async markMessageAsRead(@Param('messageId') messageId: string): Promise<Message> {
+  async markMessageAsRead(
+    @Param('messageId') messageId: string,
+  ): Promise<Message> {
     return this.chatService.markMessageAsRead(messageId);
   }
 }
