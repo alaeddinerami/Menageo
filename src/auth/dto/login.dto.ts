323425e7c2bce9e22bbe79c98cbea226b0d034b7
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 23:23:15 +0000

    Mise à jour des DTOs d'authentification

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,13 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    readonly email: string;
-
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(6)
-    readonly password: string
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  readonly password: string;
 }
