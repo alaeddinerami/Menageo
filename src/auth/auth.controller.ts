a34ab00219878b7aec1be453d96ca5ed35a63440
alaeddinerami <alaeddinerami7@gmail.com>
2025-03-19 21:43:21 +0000

    Mise à jour du contrôleur d'authentification

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { single } from 'rxjs';
 import { LoginDto } from './dto/login.dto';
+import { ImageUploadInterceptor } from 'src/common/middleware/multer.middleware';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signUp')
-  SugnUp(@Body() SignupDto: SignupDto){
-    return this.authService.signUp(SignupDto)
+   @UseInterceptors(ImageUploadInterceptor())
+  SugnUp(@Body() SignupDto: SignupDto,@UploadedFile() image: Express.Multer.File){
+    console.log(SignupDto)
+    return this.authService.signUp(SignupDto,image)
   }
   @Post('login')
   login(@Body() LoginDto: LoginDto){
